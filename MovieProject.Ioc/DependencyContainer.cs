316e7259ce7a1da00851572e578d2a31e923c490
Alim03 <ali.mohamadkhani03@gmail.com>
2023-02-02 15:23:38 +0330

    Initial

---
+++ MovieProject.Ioc/DependencyContainer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MovieProject.Ioc
+{
+    public static class DependencyContainer
+    {
+        public static void AddDependencies(this IServiceCollection services) { }
+    }
+}
